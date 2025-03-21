a5fce44d1b8f62bacc277910b80bfe5474102b58
Poorvi Kanodia <poorvikanodia19@gmail.com>
2025-03-21 22:16:56 +0530

    initial commit

---
+++ main.js
@@ -0,0 +1,17 @@
+require("dotenv").config();
+const express=require("express");
+const app=express();
+const PORT =process.env.PORT || 4000;
+
+app.use(express.urlencoded({extended: false}))
+app.use(express.json())
+
+app.use('/',require("./routes/routes"));
+app.set('view engine', 'ejs');
+
+app.listen(PORT, ()=>{
+    console.log(`Server started at http://localhost:${PORT}`);
+});
+
+app.use(express.static('css'));
+app.use(express.static('scripts'));
